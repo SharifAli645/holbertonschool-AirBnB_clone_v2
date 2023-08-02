2a0dbf3954c4bfb4a0eefcad78804cc8a333df56
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-01 21:24:52 -0700

    select all tables in the db

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -11,4 +11,4 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grant select privilege
-GRANT SELECT ON performance_schema TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
