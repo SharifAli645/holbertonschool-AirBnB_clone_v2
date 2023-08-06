c4f1597d89244ccde3b4755f01e47f3dee554b0c
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-06 11:40:43 -0700

    feat: create a class depending on the input

--- models/user.py
+++ models/user.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+if getenv("HBN_TYPE_STORAGE") == "db":
+    class User(BaseModel, Base):
+        """This class defines a user by various attributes"""
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+
+        places = relationship("Place", backref="user", cascade="all,
+                              delete-orphan")
+        reviews = relationship("Review", backref="user", cascade="all,
+                               delete-orphan")
+
+elif getenv("HBN_TYPE_STORAGE") != "db":
+    class User(Basemodel):
+        """This class defines a user by various attributes"""
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
