4b547dc15b9b122be54c56eab5b45fd7dfb0f52d
SharifAli645 <120497885+SharifAli645@users.noreply.github.com>
2023-08-06 22:54:26 -0500

    fit: alter order of creation (db)

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -53,7 +53,7 @@ class DBStorage:
 
     def reload(self):
         """create all tables in the database and the session"""
-        Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         self.__session = scoped_session(session_factory)
+        Base.metadata.create_all(self.__engine)
