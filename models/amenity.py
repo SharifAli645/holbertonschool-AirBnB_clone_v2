2c71c0da01d7bcfdd6a583b6587015e64355d18c
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-06 17:23:51 -0700

    new classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    class Amenity(BaseModel, Base):
+        """Class that inherits from BaseModel and Base"""
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
+else:
+    class Amenity(BaseModel):
+        """Class that inherits from BaseModel"""
+        name = ""
