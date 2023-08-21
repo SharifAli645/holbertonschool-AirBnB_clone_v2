91e2b872a987d4d378542fae8741ee658b7a5c65
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-21 14:29:17 -0700

    fit: correct strings

--- models/user.py
+++ models/user.py
@@ -6,10 +6,10 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-if getenv("HBN_TYPE_STORAGE") == "db":
+if getenv("HBNB_TYPE_STORAGE") == "db":
     class User(BaseModel, Base):
         """This class defines a user by various attributes"""
-        __tablename__ = "users"
+        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
