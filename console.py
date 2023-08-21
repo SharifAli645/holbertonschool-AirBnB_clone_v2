59ae8ae57575c966fff319d3790896c685276ed1
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-21 14:31:38 -0700

    feat: also create with dbstorage

--- console.py
+++ console.py
@@ -211,18 +211,24 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
+        from os import getenv
         print_list = []
+
+        if getenv("HBNB_TYPE_STORAGE") == 'db':
+            store = storage.all(eval(args))
+        else:
+            store = storage._FileStorage__objects
 
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in store.items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
