07e6f0902bd6e910a9e972dbfecb58a855f93d94
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-23 12:40:25 -0700

    fit :correct argument for render

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,17 +9,14 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route("/states_list", strict_slashes=False)
 def states():
-    state_list = storage.all(State)
-    string = ""
-    for state in state_list:
-        string += f'<LI>{state.id}: <B>{state.name}</B></LI>'
-    return render_template("7-states_list.html", strg=string)
+    return render_template("7-states_list.html", data=storage.all(State))
 
 
 @app.teardown_appcontext
-def tear():
+def tear(exception=None):
     storage.close()
 
 
